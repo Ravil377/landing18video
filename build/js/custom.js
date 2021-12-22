d57d0ff9cba3d5fc2556d5ddd6311aa242d19962
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2021-12-22 09:48:41 +0500

    fix

--- build/js/custom.js
+++ build/js/custom.js
@@ -5617,64 +5617,63 @@
       }
     };
 
-    Swiper$1.use([Navigation$1]);
-    const movieBd = [{
-      id: 0,
-      image: "./img/content/olivia.jpg",
-      fullName: "Olivia Da Silva",
-      name: "Olivia",
-      movie: "./video/1.mp4",
-      subscribe: "1,081 <span>posts</span>2,226k <span>followes</span>"
-    }, {
-      id: 1,
-      image: "./img/content/mirela.jpg",
-      fullName: "Mirela Flores",
-      name: "Mirela",
-      movie: "./video/2.mp4",
-      subscribe: "2,122 <span>posts</span>5,340k <span>followes</span>"
-    }, {
-      id: 2,
-      image: "./img/content/barbara.jpg",
-      fullName: "Bárbara Andrade",
-      name: "Bárbara",
-      movie: "./video/3.mp4",
-      subscribe: "1,345 <span>posts</span>3,546k <span>followes</span>"
-    }];
+    Swiper$1.use([Navigation$1]); // const movieBd = [
+    //     {
+    //         id: 0,
+    //         image: "./img/content/olivia.jpg",
+    //         fullName: "Olivia Da Silva",
+    //         name: "Olivia",
+    //         movie: "./video/1.mp4",
+    //         subscribe: "1,081 <span>posts</span>2,226k <span>followes</span>"
+    //     },
+    //     {
+    //         id: 1,
+    //         image: "./img/content/mirela.jpg",
+    //         fullName: "Mirela Flores",
+    //         name: "Mirela",
+    //         movie: "./video/2.mp4",
+    //         subscribe: "2,122 <span>posts</span>5,340k <span>followes</span>"
+    //     },
+    //     {
+    //         id: 2,
+    //         image: "./img/content/barbara.jpg",
+    //         fullName: "Bárbara Andrade",
+    //         name: "Bárbara",
+    //         movie: "./video/3.mp4",
+    //         subscribe: "1,345 <span>posts</span>3,546k <span>followes</span>"
+    //     },
+    // ]
+
     let count = 0;
-    const movieContainer = document.querySelector('.movie__list');
-    const movieCard = document.querySelector('#movieCard');
+    const movieContainer = document.querySelector('.movie__list'); // const movieCard = document.querySelector('#movieCard'); 
+    // function loadCards(container, data) { 
+    //     const cardsElements = data.map(getCard); 
+    //     container.append(...cardsElements); 
+    // } 
+    // function shuffle(sourceArray) {
+    //     for (var i = 0; i < sourceArray.length - 1; i++) {
+    //         var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+    //         var temp = sourceArray[j];
+    //         sourceArray[j] = sourceArray[i];
+    //         sourceArray[i] = temp;
+    //     }
+    //     return sourceArray;
+    // }
+    // function getCard(card) { 
+    //     const cardElement = movieCard.content.cloneNode(true);
+    //     const videoContainer = cardElement.querySelector(".movie__item"); 
+    //     const cardVideo = cardElement.querySelector(".js-video-src"); 
+    //     videoContainer.dataset.id = card.id;
+    //     videoContainer.dataset.name = card.name;
+    //     videoContainer.dataset.fullname = card.fullName;
+    //     videoContainer.dataset.img = card.image;
+    //     videoContainer.dataset.subscribe = card.subscribe;
+    //     cardVideo.src = card.movie; 
+    //     return cardElement; 
+    // } 
+    // const newMas = shuffle(movieBd);
+    // loadCards(movieContainer, newMas);
 
-    function loadCards(container, data) {
-      const cardsElements = data.map(getCard);
-      container.append(...cardsElements);
-    }
-
-    function shuffle(sourceArray) {
-      for (var i = 0; i < sourceArray.length - 1; i++) {
-        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
-        var temp = sourceArray[j];
-        sourceArray[j] = sourceArray[i];
-        sourceArray[i] = temp;
-      }
-
-      return sourceArray;
-    }
-
-    function getCard(card) {
-      const cardElement = movieCard.content.cloneNode(true);
-      const videoContainer = cardElement.querySelector(".movie__item");
-      const cardVideo = cardElement.querySelector(".js-video-src");
-      videoContainer.dataset.id = card.id;
-      videoContainer.dataset.name = card.name;
-      videoContainer.dataset.fullname = card.fullName;
-      videoContainer.dataset.img = card.image;
-      videoContainer.dataset.subscribe = card.subscribe;
-      cardVideo.src = card.movie;
-      return cardElement;
-    }
-
-    const newMas = shuffle(movieBd);
-    loadCards(movieContainer, newMas);
     document.addEventListener('DOMContentLoaded', function () {
       const containerButtons = document.querySelectorAll('.container__buttons');
       const name = document.querySelector('.info__name');
